764fd2d3f8b4c904549616ef26059f7b317b34b9
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-10-08 20:10:02 +0600

    add brand worp

--- asstec/js/main.js
+++ asstec/js/main.js
@@ -163,6 +163,50 @@ $(document).ready(function () {
   });
 //=====================countundoen ========//
  
+
+$('.latest_news_slied').slick({
+  arrows: false,
+  autoplay: true,
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        dots: false,
+      }
+    }
+  ]
+});
+//=====================countundoen ========//
+
+
+
     const second = 1000,
     minute = second * 60,
     hour = minute * 60,
