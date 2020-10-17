984c4f5df7f7634c7bae70d6c3b52c28f830c24b
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-10-17 18:50:21 +0600

    all video magnific-popup js add

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
   //header js//
   $(window).scroll(function () {
@@ -123,9 +121,10 @@ $(document).ready(function () {
   //===============instgam==========//
   $('.slider_main, .brabd_slied').slick({
     arrows: true,
-    autoplay: false,
+    autoplay: true,
     dots: false,
     infinite: true,
+    centerMode: true,
     speed: 300,
     slidesToShow: 5,
     slidesToScroll: 3,
@@ -139,6 +138,46 @@ $(document).ready(function () {
       },
       {
         breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          dots: false,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+        }
+      }
+    ]
+  });
+  //=====================countundoen ========//
+  $('.latest_news_slied').slick({
+    arrows: false,
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           dots: false,
@@ -160,70 +199,30 @@ $(document).ready(function () {
       }
     ]
   });
-  //=====================countundoen ========//
-    $('.latest_news_slied').slick({
-      arrows: false,
-      autoplay: true,
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [{
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            dots: false,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            dots: false,
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            dots: false,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            dots: false,
-          }
-        }
-      ]
-    });
 
 
 
-  var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+  var countDownDate = new Date("Jan 1, 2021 15:37:25").getTime();
   // Update the count down every 1 second
-  var x = setInterval(function() {
+  var x = setInterval(function () {
 
     // Get today's date and time
     var now = new Date().getTime();
-      
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-      
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
+
     // Output the result in an element with id="demo"
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("min").innerHTML = minutes;
     document.getElementById("sec").innerHTML = seconds;
-      
+
     // If the count down is over, write some text 
     if (distance < 0) {
       clearInterval(x);
@@ -231,90 +230,90 @@ $(document).ready(function () {
     }
   }, 1000);
 
-    
-  //========== filtering========== //
 
-  // function isoTopActive() {
-  //   // Iso top js
-  //   $(".filtering_item_row").isotope({
-  //       itemSelector: '.custom_col',
-  //       layoutMode: 'fitRows'
-  //   });
-  //   var isOacTive = $('.isotopmein button').click(function () {
-  //       $(".isotopmein button").removeClass("active");
-  //       $(this).addClass("active");
-  //       var selector = $(this).attr('data-filter');
-  //       $(".filtering_item_row").isotope({
-  //           filter: selector,
-  //           animationOptions: {
-  //               duration: 750,
-  //               easing: 'linear',
-  //               queue: false,
-  //           }
-  //       });
-  //       return false;
-  //   });
-
-  // }
-  // isoTopActive();
-  //       /*---------LIGHT-BOX js-----------*/
-  // function lightBoxImages() {
-  //   var selectorG = '.icon_link';
-  //   if($(selectorG).length){
-  //       var instanceG = $(selectorG).imageLightbox({
-  //           quitOnDocClick:	false,
-  //           button:         true,
-  //           activity:       true,
-  //           overlay:        true,
-  //           arrows:         true,
-  //           preloadNext:    true,
-  //       });		
-  //   }
-  // }
-  // lightBoxImages();
-
-
+  // ========== filtering========== //
 
   function isoTopActive() {
     // Iso top js
-    $(".ctm_row").isotope({
-        itemSelector: '.ctm_col',
-        layoutMode: 'fitRows'
+    $(".filtering_item_row").isotope({
+      itemSelector: '.custom_col',
+      layoutMode: 'fitRows'
     });
 
-    var isOacTive = $('.Isotopemanu button').click(function () {
-        $(".Isotopemanu button").removeClass("active");
-        $(this).addClass("active");
-        var selector = $(this).attr('data-filter');
-        $(".ctm_row").isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false,
-            }
-        });
-        return false;
+    var isOacTive = $('.isotopmein button').click(function () {
+      $(".isotopmein button").removeClass("active");
+      $(this).addClass("active");
+      var selector = $(this).attr('data-filter');
+      $(".filtering_item_row").isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false,
+        }
+      });
+      return false;
     });
   }
   isoTopActive();
 
-        /*---------LIGHT-BOX js-----------*/
+  /*---------LIGHT-BOX js-----------*/
   function lightBoxImages() {
     var selectorG = '.icon_link';
-    if($(selectorG).length){
-        var instanceG = $(selectorG).imageLightbox({
-            quitOnDocClick:	false,
-            button:         true,
-            activity:       true,
-            overlay:        true,
-            arrows:         true,
-            preloadNext:    true,
-        });		
+    if ($(selectorG).length) {
+      var instanceG = $(selectorG).imageLightbox({
+        quitOnDocClick: false,
+        button: true,
+        activity: true,
+        overlay: true,
+        arrows: true,
+        preloadNext: true,
+      });
     }
   }
   lightBoxImages();
 
+
+
+  // function isoTopActive1() {
+  //   // Iso top js
+  //   $(".ctm_row").isotope({
+  //     itemSelector: '.ctm_col',
+  //     layoutMode: 'fitRows'
+  //   });
+
+  //   var isOacTive = $('.Isotopemanu button').click(function () {
+  //     $(".Isotopemanu button").removeClass("active");
+  //     $(this).addClass("active");
+  //     var selector = $(this).attr('data-filter');
+  //     $(".ctm_row").isotope({
+  //       filter: selector,
+  //       animationOptions: {
+  //         duration: 750,
+  //         easing: 'linear',
+  //         queue: false,
+  //       }
+  //     });
+  //     return false;
+  //   });
+  // }
+  // isoTopActive1();
+
+  /*---------LIGHT-BOX js-----------*/
+  function lightBoxImages() {
+    var selectorG ='.icon_link';
+    if ($(selectorG).length) {
+      var instanceG = $(selectorG).imageLightbox({
+        quitOnDocClick:	false,
+        button:         true,
+        activity:       true,
+        overlay:        true,
+        arrows:         true,
+        preloadNext:    true,
+      });
+    }
+  }
+  lightBoxImages();
 
 
   // pop up
@@ -322,14 +321,25 @@ $(document).ready(function () {
 
   function videoPopup() {
     if ($('.popup-youtube').length) {
-        $('.popup-youtube').magnificPopup({
-            type: 'iframe',
-            // gallery:{
-            //   enabled:true
-            // }
-        });
+      $('.popup-youtube').magnificPopup({
+        type: 'iframe',
+        // gallery:{
+        //   enabled:true
+        // }
+      });
     }
-}
-videoPopup();
+  }
+  videoPopup();
+
+  // up icon js //
+  $('.up_icon a[href^="#"').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 2000);
+    }
+  });
 
 });
