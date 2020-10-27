66d75ca579bb33c2b2111eb9b8603d12f0c538b8
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-10-27 16:11:21 +0600

    add speed 300, banner js

--- asset/js/main.js
+++ asset/js/main.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
         autoplay: true
         , arrows: true
         , autoplaySpeed: 3000,
+        speed : 3000,
         easing: 'linear'
       });
     }(jQuery)
