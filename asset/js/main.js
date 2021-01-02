a0e4df6697a752c1065a0662a7dbfa5766290714
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2021-01-03 00:25:23 +0600

    add counntdown id spec removed

--- asset/js/main.js
+++ asset/js/main.js
@@ -217,34 +217,39 @@ $(document).ready(function() {
     });
 
 
-    //=====================countundoen =================//
-    var countDownDate = new Date("Jan 1, 2021 15:37:25").getTime();
-    // Update the count down every 1 second
-    var x = setInterval(function() {
+    function timerCounter() {
 
-        // Get today's date and time
-        var now = new Date().getTime();
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
+        //=====================countundoen =================//
+        var countDownDate = new Date("Jan 1, 2022 15:37:25").getTime();
+        // Update the count down every 1 second
+        var x = setInterval(function() {
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            // Get today's date and time
+            var now = new Date().getTime();
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
 
-        // Output the result in an element with id="demo"
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("min").innerHTML = minutes;
-        document.getElementById("sec").innerHTML = seconds;
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("countDown").innerHTML = "EXPIRED";
-        }
-    }, 1000);
+            // Output the result in an element with id="demo"
+            document.getElementById("days").innerHTML = days;
+            document.getElementById("hours").innerHTML = hours;
+            document.getElementById("min").innerHTML = minutes;
+            document.getElementById("sec").innerHTML = seconds;
+
+            // If the count down is over, write some text 
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countDown").innerHTML = "EXPIRED";
+            }
+        }, 1000);
+    }
+
+    timerCounter();
 
     // ========== filtering========== //
     function isoTopActive() {
